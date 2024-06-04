c35376dd99700cbb4aa1ebb8dcbbdd573ef5af68
Dimanskr <dimanskr2@gmail.com>
2024-06-04 23:33:37 +0300

    add hide card number and output functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from settings import OPERATIONS_FILE, OPERATIONS_COUNT
 from utils.helper import get_operations_by_status, get_last_operations
 from utils.loader import get_list_from_json_file
+from views.mask import output_operation_detail
 
 
 def main():
@@ -10,6 +11,9 @@ def main():
     operations = get_operations_by_status(operations_list, status="EXECUTED")
     # список последних операций
     last_operations = get_last_operations(operations, OPERATIONS_COUNT)
+    # выводим операции
+    for operation in last_operations:
+        print(output_operation_detail(operation))
 
 
 if __name__ == '__main__':
